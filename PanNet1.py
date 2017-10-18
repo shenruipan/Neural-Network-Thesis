77afb8c149e0e71a73007917b643cf4e6adb0ea4
shenruipan <shenrui@uchicago.edu>
2017-10-17 22:59:40 -0500

    PanNet-enlarge

--- PanNet1.py
+++ PanNet1.py
@@ -11,12 +11,12 @@ tf.set_random_seed(123)
 np.random.seed(123)
 
 # Parameters
-num_filter1 = 4
-num_filter2 = 12
+num_filter1 = 100
+num_filter2 = 120
 learning_rates = 0.1
-num_training1 = 3000
-num_training2 = 3000
-num_training3 = 7500
+num_training1 = 1000
+num_training2 = 1000
+num_training3 = 10000
 
 # Default parameters
 num_input = 784
