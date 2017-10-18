68ae38e0c51b22060b92f1dd4046f4a9583ca81a
shenruipan <shenrui@uchicago.edu>
2017-10-17 19:06:39 -0500

    PanNet1-enlarge number of training 1

--- PanNet1_searching_num_train1.py
+++ PanNet1_searching_num_train1.py
@@ -14,9 +14,9 @@ tf.set_random_seed(123)
 np.random.seed(123)
 
 # Parameters
-num_filter1 = 4
+num_filter1 = 100
 learning_rates = 0.1
-num_training1 = 20000
+num_training1 = 5000
 
 # Default parameters
 num_input = 784
